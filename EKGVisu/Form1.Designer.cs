e1b05de91703a3fe551f29d5132679622bd20d99
Milan Polacek <polacek.emilan@gmail.com>
2014-03-12 16:27:06 +0100

    Uvodni comit

---
+++ EKGVisu/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace EKGVisu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            this.btnReadCSV = new System.Windows.Forms.Button();
+            this.btnReadTxT = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.Draw = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnReadCSV
+            // 
+            this.btnReadCSV.Location = new System.Drawing.Point(584, 546);
+            this.btnReadCSV.Name = "btnReadCSV";
+            this.btnReadCSV.Size = new System.Drawing.Size(75, 23);
+            this.btnReadCSV.TabIndex = 0;
+            this.btnReadCSV.Text = "Nacti CSV";
+            this.btnReadCSV.UseVisualStyleBackColor = true;
+            this.btnReadCSV.Click += new System.EventHandler(this.btnReadCSV_Click);
+            // 
+            // btnReadTxT
+            // 
+            this.btnReadTxT.Location = new System.Drawing.Point(503, 546);
+            this.btnReadTxT.Name = "btnReadTxT";
+            this.btnReadTxT.Size = new System.Drawing.Size(75, 23);
+            this.btnReadTxT.TabIndex = 1;
+            this.btnReadTxT.Text = "Nacti TxT";
+            this.btnReadTxT.UseVisualStyleBackColor = true;
+            this.btnReadTxT.Click += new System.EventHandler(this.btnReadTxT_Click);
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(20, 22);
+            this.chart1.Name = "chart1";
+            this.chart1.Size = new System.Drawing.Size(1094, 518);
+            this.chart1.TabIndex = 2;
+            this.chart1.Text = "chart1";
+            // 
+            // Draw
+            // 
+            this.Draw.Location = new System.Drawing.Point(422, 546);
+            this.Draw.Name = "Draw";
+            this.Draw.Size = new System.Drawing.Size(75, 23);
+            this.Draw.TabIndex = 3;
+            this.Draw.Text = "Kreslit";
+            this.Draw.UseVisualStyleBackColor = true;
+            this.Draw.Click += new System.EventHandler(this.Draw_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(949, 568);
+            this.Controls.Add(this.Draw);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.btnReadTxT);
+            this.Controls.Add(this.btnReadCSV);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnReadCSV;
+        private System.Windows.Forms.Button btnReadTxT;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button Draw;
+    }
+}
+
---
+++ EKGVisu/Form1.cs
@@ -0,0 +1,304 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using System.IO;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace EKGVisu
+{
+    public partial class Form1 : Form
+    {
+        private string[] dataFromCSVFile;
+        private string[] tresholdData;
+        private string[] dataFromTxTFile;
+        private string nameRowEKG = "EKG";
+        private string nameRowRR = "RR";
+        private string nameRowKPeak = "Peak";
+
+        //Inicializacni cast pro grafy
+        public Form1()
+        {
+            // inicializace grafu
+            InitializeComponent();
+            chart1.ChartAreas[0].AxisX.ScaleView.Zoomable = true;
+            chart1.ChartAreas[0].AxisY.ScaleView.Zoomable = true;
+
+            chart1.ChartAreas[0].CursorX.AutoScroll = true;
+            chart1.ChartAreas[0].CursorY.AutoScroll = true;
+
+            chart1.ChartAreas[0].CursorX.IsUserSelectionEnabled = true;
+            chart1.ChartAreas[0].CursorY.IsUserSelectionEnabled = true;
+        }
+
+        ///Tlacitko pro nacteni TxT 
+        private void btnReadTxT_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    ReadDataFromFileTxT(openFileDialog1.FileName);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Pojebalo se to nekde pri nacitani Puvodni error: " + ex.Message);
+                }
+            }
+        }
+
+        ///Tlacitko pro nacteni CSV
+        private void btnReadCSV_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "txt files (*.csv)|*.csv|All files (*.*)|*.*";
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    ReadDataFromFileCSV(openFileDialog1.FileName);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Pojebalo se to nekde pri nacitani Puvodni error: " + ex.Message);
+                }
+            }
+
+        }
+
+        /// Tlacitko pro vykresleni dat
+        private void Draw_Click(object sender, EventArgs e)
+        {
+            // foreach (var series in chart1.Series)
+            // {
+            //     series.Points.Clear();
+            // }
+
+            double[] doubleArrayCSV = ConvertToDoubleArray(dataFromCSVFile);
+            DrawEKG(doubleArrayCSV, nameRowEKG);
+            DrawKFiltredSignal(dataFromTxTFile, doubleArrayCSV, nameRowRR);
+            DrawPeak(doubleArrayCSV, nameRowKPeak);
+        }
+
+        /// <summary>
+        /// Funkce pro nacitani dat ze souboru s koncovkou csv
+        /// </summary>
+        /// <param name="route">Cesta k souboru</param>
+        private void ReadDataFromFileCSV(string route)
+        {
+            string[] rawDataFromCSVFile = File.ReadAllLines(route);
+            tresholdData = rawDataFromCSVFile[1].Split(',');
+            nameRowEKG += (" from " + rawDataFromCSVFile[0]);
+            dataFromCSVFile = rawDataFromCSVFile[2].Split(',');
+        }
+
+        /// <summary>
+        /// Funkce pro nacitani dat ze souboru s koncovkou txt
+        /// </summary>
+        /// <param name="route">Cesta k souboru</param>
+        private void ReadDataFromFileTxT(string route)
+        {
+            dataFromTxTFile = System.IO.File.ReadAllLines(route);
+        }
+
+        /// <summary>
+        /// Pretypovani dat ze stringoveho pole na double pole 
+        /// </summary>
+        /// <param name="inputStringArray">Vstupni stringove pole</param>
+        /// <returns>Vracime double pole</returns>
+        private double[] ConvertToDoubleArray(string[] inputStringArray)
+        {
+            double[] arraysOfDouble = new double[inputStringArray.Length];
+            for (int i = 0; i < inputStringArray.Length - 1; i++)
+            {
+                arraysOfDouble[i] = Convert.ToDouble(inputStringArray[i]);
+            }
+            return arraysOfDouble;
+        }
+
+        /// <summary>
+        /// Predelani separatoru pro budouci praci
+        /// </summary>
+        /// <param name="inputStringArray">Vstupni stringove pole</param>
+        /// <param name="oldSeparator">Separator kterz ma byt nahrazen</param>
+        /// <param name="newSeparator">Cim ma byt nahrazen</param>
+        /// <returns>Vraci pole stringu s novym separatorem</returns>
+        private string[] ReplaceSeparator(string[] inputStringArray, string oldSeparator, string newSeparator)
+        {
+            string[] newArrayWithNewSeparator = new string[inputStringArray.Length];
+            for (int i = 0; i < inputStringArray.Length - 1; i++)
+            {
+                newArrayWithNewSeparator[i] = inputStringArray[i].Replace(oldSeparator, newSeparator);
+            }
+            return newArrayWithNewSeparator;
+        }
+
+        /// <summary>
+        /// Vykresli EKG data
+        /// </summary>
+        /// <param name="ArrayCSV">Vstupni pole double s EKG Daty</param>
+        /// <param name="nameLine">Nazev rady</param>
+        private void DrawEKG(double[] ArrayCSV, string nameLine)
+        {
+            chart1.Series.Add(nameLine);
+            chart1.Series[nameLine].ChartType = SeriesChartType.FastLine;
+            DrawData(ArrayCSV, nameLine);
+        }
+
+        /// <summary>
+        /// Funkce pro vykresleni vyfiltrovaneho signalu
+        /// </summary>
+        /// <param name="inputStringArrayFromTxT">Vstupni pole</param>
+        /// <param name="arrayCSV">Pole double se signalem</param>
+        /// <param name="nameLine">Nazev rady</param>
+        private void DrawKFiltredSignal(string[] inputStringArrayFromTxT, double[] arrayCSV, string nameLine)
+        {
+            string[] replacedArrayTxT = ReplaceSeparator(inputStringArrayFromTxT, ".", ",");
+            double[] arrayTxT = ConvertToDoubleArray(replacedArrayTxT);
+            double[] convolutedSignalos = Convolution(arrayCSV, arrayTxT);
+            chart1.Series.Add(nameLine);
+            chart1.Series[nameLine].ChartType = SeriesChartType.FastLine;
+            DrawData(convolutedSignalos, nameLine);
+        }
+
+        private void DrawPeak(double[] ArrayCSV, string nameLine)
+        {
+            chart1.Series.Add(nameLine);
+            chart1.Series[nameLine].ChartType = SeriesChartType.Point;
+            List<int> peaks=NumeratePeaks(ArrayCSV, Convert.ToDouble(tresholdData[1]));
+            foreach (int y in peaks)
+            {
+                chart1.Series[nameLine].Points.AddXY(y, 300);  
+            }
+
+            chart1.Series.Add("Tachogram");
+            chart1.Series["Tachogram"].ChartType = SeriesChartType.Line;
+            List<int> tachogarm = Tachogram(peaks);
+            for (int i = 0; i < tachogarm.Count(); i++)
+			{
+                chart1.Series["Tachogram"].Points.AddXY(peaks[i], tachogarm[i]);
+			}
+            
+          
+        }
+
+        /// <summary>
+        /// Funkce pro vykresleni dat
+        /// </summary>
+        /// <param name="inputDataArray">Nacita vstupni double pole</param>
+        /// <param name="nameLine">Nazev rady</param>
+        private void DrawData(double[] inputDataArray, string nameLine)
+        {
+            for (int i = 0; i < inputDataArray.Length - 1; i++)
+            {
+                chart1.Series[nameLine].Points.AddXY(i, inputDataArray[i]);
+            }
+        }
+
+        private static double[] Convolution(double[] inputSignalDataArray, double[] filterData)
+        {
+            double[] convolutedSignal = new double[inputSignalDataArray.Length + filterData.Length - 1];
+
+        #region convoluce
+            if (inputSignalDataArray.Length >= filterData.Length)
+            {
+                for (int k = 0; k < inputSignalDataArray.Length + filterData.Length - 1; k++)
+                {
+                    double pom = 0f;
+                    if (k < filterData.Length - 1)
+                    {
+                        for (int i = 0; i <= k; i++)
+                            pom += inputSignalDataArray[k - i] * filterData[i];
+                    }
+                    else if (k >= filterData.Length - 1 & k <= inputSignalDataArray.Length - 1)
+                    {
+                        for (int i = 0; i <= filterData.Length - 1; i++)
+                            pom += inputSignalDataArray[k - i] * filterData[i];
+                    }
+                    else if (k > inputSignalDataArray.Length - 1)
+                    {
+                        for (int i = (k + 1) - inputSignalDataArray.Length; i <= filterData.Length - 1; i++)
+                            pom += inputSignalDataArray[k - i] * filterData[i];
+                    }
+                    convolutedSignal[k] = pom;
+                }
+            }
+
+            if (inputSignalDataArray.Length < filterData.Length)
+            {
+                for (int k = 0; k < filterData.Length + inputSignalDataArray.Length - 1; k++)
+                {
+                    double pom = 0f;
+                    if (k < inputSignalDataArray.Length - 1)
+                    {
+                        for (int i = 0; i <= k; i++)
+                            pom += filterData[k - i] * inputSignalDataArray[i];
+                    }
+                    else if (k >= inputSignalDataArray.Length - 1 & k <= filterData.Length - 1)
+                    {
+                        for (int i = 0; i <= inputSignalDataArray.Length - 1; i++)
+                            pom += filterData[k - i] * inputSignalDataArray[i];
+                    }
+                    else if (k > filterData.Length - 1)
+                    {
+                        for (int i = (k + 1) - filterData.Length; i <= inputSignalDataArray.Length - 1; i++)
+                            pom += filterData[k - i] * inputSignalDataArray[i];
+                    }
+                    convolutedSignal[k] = pom;
+                }
+            }
+#endregion
+
+            int pomItera = 0;
+            double [] finalConvolutedSignal = new double[inputSignalDataArray.Length];
+            for (int i = (int)(filterData.Length/2); i < inputSignalDataArray.Length; i++)
+            {
+                 finalConvolutedSignal[pomItera]  =  convolutedSignal[i];
+                 pomItera++;
+            }
+
+
+            return finalConvolutedSignal;
+        }
+
+        public List<int> NumeratePeaks(double[] inputSignalDataArray, double threshold)
+        {
+            List<int> spice = new List<int>();
+            for (int i = 1; i < inputSignalDataArray.Length - 1; i++)
+            {
+                if (inputSignalDataArray[i] > threshold)
+                {
+                    if (inputSignalDataArray[i - 1] < inputSignalDataArray[i] && inputSignalDataArray[i] > inputSignalDataArray[i + 1])
+                    {
+                        spice.Add(i);
+                    }
+                }
+            }
+            return spice;
+        }
+
+        private static List<int> Tachogram(List<int> peaks)
+        {
+            List<int> tachogram = new List<int>();
+
+            for (int i = 0; i < peaks.Count()-1; i++)
+            {
+                tachogram.Add(peaks[i+1] - peaks[i]);
+            }
+            return tachogram;
+        }
+
+
+    }
+}
